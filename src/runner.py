c236c81ab85aca4e9a7e870c9b0486881057c61f
Rik van Loenhout <code@rkvl.org>
2024-10-08 11:10:03 +0200

    Update runner

--- src/runner.py
+++ src/runner.py
@@ -1,15 +1,55 @@
 import runpod
+import json
+import subprocess
 
 def handler(job):
     job_input = job['input']
 
+    # Make data folder writeable
+    cmd = '''
+        mkdir /data
+        chmod 777 /data
+    '''
+    subprocess.check_output(cmd, shell=True)
+
+    # Add SSH key
+    cmd = '''
+        mkdir -p ~/.ssh
+        echo "{ssh_key}" >> ~/.ssh/authorized_keys
+        chmod 700 -R ~/.ssh
+        generate_ssh_keys
+        service ssh start 
+    '''.format(ssh_key=job_input["ssh_key"])
+    subprocess.check_output(cmd, shell=True)
+
+    # Compile handler code
     handler = job_input.get('handler', '')
-    data = job_input.get('data', '')
-
     code = compile(handler, 'handler', 'exec')
-    output = exec(code)
+    exec(code)
 
-    return f"Hello, {output}!"
+    # Parse data
+    path = job_input.get('data', '')
+    with open(path) as json_file:
+        data = json.load(json_file)
+
+    try:
+        # Start runner
+        model = Runner.start(
+            {
+                "path": "/data/"
+            },
+            data
+        )
+
+        return json.dumps({
+            "success": True,
+            "model": model,
+        })
+    except Exception as e:
+        return json.dumps({
+            "success": False,
+            "error": str(e)
+        })
 
 
 runpod.serverless.start({"handler": handler})
